23ba2963666233555d9a6ad32ed3533b80c2c2bf
ivar <ivar.vanhartingsveldt@inbo.be>
2022-09-09 15:20:37 +0200

    - Added separate loading state for the data table when you switch between pages, or during filtering.

--- projects/ng-inbo/src/lib/services/api/request-state.enum.ts
+++ projects/ng-inbo/src/lib/services/api/request-state.enum.ts
@@ -1,6 +1,8 @@
 export enum RequestState {
   DEFAULT = 'DEFAULT',
   PENDING = 'PENDING',
+  // ⬇ This state is needed to be able to make the difference between first page load and loading in between filtering / sorting / changing pages
+  PARTIAL_PENDING = 'PARTIAL_PENDING',
   SUCCESS = 'SUCCESS',
   ERROR = 'ERROR',
   EMPTY = 'EMPTY',
